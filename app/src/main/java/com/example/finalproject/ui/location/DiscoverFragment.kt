d1717fddda217a57f5953cd9675d5c9186f37949
JessieGullo <129686666+Gulloje@users.noreply.github.com>
2024-04-29 12:44:54 -0400

    added recycler view

--- app/src/main/java/com/example/finalproject/ui/location/DiscoverFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/location/DiscoverFragment.kt
@@ -2,26 +2,14 @@ package com.example.finalproject.ui.location
 
 
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Location
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.finalproject.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import java.util.concurrent.TimeUnit
+
 
 
 
